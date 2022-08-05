c4fc60e4213f647b9b265a8162ca1eece4b31fbe
jfugon <jennifer.guerrero@helloiconic.com>
2022-08-05 14:21:24 -0600

    changes

--- specs/example.test.js
+++ specs/example.test.js
@@ -20,7 +20,7 @@ describe("Visual test Aj page", function () {
     await browser.url(HOME_URL);
     await expect(await browser.getTitle()).toEqual("Aj");
     expect(await AJPage.selectHomeItem()).toBeDisplayed(true);
-    await percySnapshot(browser, "homepage page");
+    await percySnapshot("homepage page");
   });
   it("the user clicks the jewerly item", async function () {
     await browser.url(HOME_URL);
@@ -29,12 +29,19 @@ describe("Visual test Aj page", function () {
   it("the user clicks the contact item", async function () {
     await browser.url(HOME_URL);
     await AJPage.selectContactItem();
-    /*await percySnapshot(browser, "contact section", {
+    /*----Screenshot a single element------*/
+
+    /*await percySnapshot("contact section", {
       scope: ".contact",
     });*/
   });
   it("the user clicks the clients item", async function () {
     await browser.url(HOME_URL);
     await AJPage.selectClientItem();
+    /*---Percy specific CSS example-----*/
+
+    /*await percySnapshot("homepage page", {
+      percyCSS: `.logo { display: none;} `,
+    });*/
   });
 });
