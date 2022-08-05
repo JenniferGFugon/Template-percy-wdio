ec39f3bebc29991de213c6d23f440d98b8525146
jfugon <jennifer.guerrero@helloiconic.com>
2022-08-04 18:43:53 -0600

    cambios

--- specs/example.test.js
+++ specs/example.test.js
@@ -1,7 +1,6 @@
 const httpServer = require("http-server");
 const percySnapshot = require("@percy/webdriverio");
 const AJPage = require("../pages/home.page");
-
 describe("Visual test Aj page", function () {
   const PORT = 8000;
   const HOME_URL = `http://localhost:${PORT}`;
